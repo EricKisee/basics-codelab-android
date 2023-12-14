8e87608efd1cfb90c10a19d51d3b579dc83d7115
EricKisee <erickiseemulwa@gmail.com>
2023-12-15 00:56:51 +0300

    Jetpack compose basics

--- app/src/main/java/com/erickisee/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/erickisee/basicscodelab/MainActivity.kt
@@ -1,21 +1,42 @@
 package com.erickisee.basicscodelab
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ElevatedButton
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import com.erickisee.basicscodelab.ui.theme.BasicsCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,40 +44,119 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BasicsCodelabTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    MyApp()
-                }
+                MyApp(modifier = Modifier.fillMaxSize())
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(
-        color = MaterialTheme.colorScheme.secondary,
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
-    ){
-        Row (modifier = Modifier.padding(24.dp)){
-            Column (
-                modifier = Modifier.weight(1f).padding(horizontal = 10.dp)
-            ){
-                Text(text = "Hello $name!" )
-                Text(text = "Are you having a good day?")
-            }
-            ElevatedButton(onClick = { /*TODO*/ }) {
-                Text(text = "Show More")
-            }
+fun MyApp (modifier: Modifier=Modifier){
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+
+    Surface (modifier, color =  MaterialTheme.colorScheme.background) {
+        if (shouldShowOnboarding) {
+            OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false})
+        }else{
+            Greetings()
         }
-
     }
-
 }
 
+@Composable
+fun OnboardingScreen (
+    onContinueClicked:()->Unit,
+    modifier: Modifier=Modifier
+){
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text("Welcome to the Basics Codelab!")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked
+        ) {
+            Text("Continue")
+        }
+    }
+}
+
+@Composable
+fun Greetings (
+    names: List <String> = List(1000) {"$it"}
+){
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = names){ name ->
+            Greeting(name = name)
+        }
+    }
+}
+
+@Composable
+fun Greeting (name: String){
+    Card (
+        colors = CardDefaults.cardColors(
+            containerColor =  MaterialTheme.colorScheme.primary
+        ),
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ){
+        CardContent(name)
+    }
+}
+
+
+@Composable
+fun CardContent(name: String, modifier: Modifier = Modifier) {
+    var expanded by remember { mutableStateOf(false) }
+    
+    Row (
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+    ){
+        Column (
+            modifier = Modifier
+                .weight(1f)
+                .padding(12.dp)
+        ){
+            Text(text = "Hello" )
+            Text(
+                text = "$name!", 
+                style=MaterialTheme.typography.headlineLarge.copy(fontWeight = FontWeight.ExtraBold)
+            )
+            if (expanded){
+                Text(
+                    text = ("Composem ipsum color sit lazy, padding theme elit, sed do bouncy. ")
+                        .repeat(4)
+                )
+            }
+        }
+        IconButton(onClick = { expanded = !expanded }) {
+            Icon(
+                imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                contentDescription = if (expanded) {
+                    stringResource(id = R.string.show_less)
+                } else {
+                    stringResource(id = R.string.show_more)
+                }
+            )
+        }
+    }
+}
+
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    name = "GreetingPreview Dark"
+)
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun GreetingPreview() {
@@ -65,13 +165,26 @@ fun GreetingPreview() {
     }
 }
 
+@Preview
 @Composable
-fun MyApp (
-    modifier: Modifier = Modifier,
-    names: List <String> = listOf("Elon Musk", "Warren Buffet")
-){
-    Column(modifier = Modifier.padding(vertical = 4.dp)) {
-       for (name in names){
-           Greeting(name = name)                   }
+fun MyAppPreview(){
+    BasicsCodelabTheme {
+        MyApp(Modifier.fillMaxSize())
+    }
+}
+
+@Preview
+@Composable
+fun GreetingsPreview () {
+    BasicsCodelabTheme {
+        Greetings()
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview () {
+    BasicsCodelabTheme {
+        OnboardingScreen(onContinueClicked = {})
     }
 }
